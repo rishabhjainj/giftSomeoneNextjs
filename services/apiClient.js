879980ca448996bf3f1e110858c5e702ca9a8c37
Rishabh <jrishabh252@gmail.com>
2021-04-27 13:54:13 +0530

    added https

--- services/apiClient.js
+++ services/apiClient.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 let urls = {
   test: `http://localhost:8002/`,
-  production: `http://65.2.38.72/api/`,
+  production: `https://65.2.38.72/api/`,
 };
 const api = Axios.create({
   baseURL: urls[process.env.config],
