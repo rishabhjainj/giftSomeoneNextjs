705227311cefe350b1c40032f2b7a59b8966d98b
Rishabh <jrishabh252@gmail.com>
2021-04-27 14:07:28 +0530

    url change

--- services/apiClient.js
+++ services/apiClient.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 let urls = {
-  test: `http://localhost:8002/`,
-  production: `https://65.2.38.72/api/`,
+  test: `http://65.2.38.72/api/`,
+  production: `http://65.2.38.72/api/`,
 };
 const api = Axios.create({
   baseURL: urls[process.env.config],
